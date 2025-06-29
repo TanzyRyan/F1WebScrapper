df69e9a6d452e01cf17aefded074e9a0726a2550
Tannnnzy <ryantanyz@hotmail.com>
2025-06-29 21:20:40 +1000

    updated comments and docstrings for clarity

--- main.py
+++ main.py
@@ -6,89 +6,107 @@ from scraper import scraping
 
 
 def submit():
+    '''
+    handles the event when submit button is clicked
+    '''
+    # get the year input
     user_input = entry.get()
+    # get the category input, either drivers or team
     selected_category = categories.get()
+    # scrap the webpage for the relevant information
     result_dict = scraping(user_input, selected_category)
 
+    # clear error message displayed
     error_label.config(text='')
 
+    # handle case there year is missing or is not valid
     if result_dict == None:
         error_label.config(text='Missing input')
     elif result_dict == False:
         error_label.config(text='Not a valid year')
     else:
+        # update the table if there is a valid result
         update_table(result_dict, selected_category)
 
 
 
 def update_table(result_dict, selected_category):
+    '''
+    update the table displayed on the GUI
+
+    input:
+        - result_dict: the dictionary containing the driver or team results
+        - selected_category: the selected category, either drivers or team
+    '''
+    
     if selected_category == 'drivers':
-        team_table.pack_forget()  # Hide team table
-        driver_table.pack()  # Show driver table
+        # hide the team table and display the driver table 
+        team_table.pack_forget() 
+        driver_table.pack() 
         
+        # clear the existing rows and readjust the table height
         driver_table.delete(*driver_table.get_children())
         driver_table.config(height=len(result_dict))
 
+        # insert driver's information into the table 
         for key, values in result_dict.items():
             driver_table.insert('', 'end', values=(key, values['driver'], values['team'], values['points']))
     
     elif selected_category == 'team':
-        driver_table.pack_forget()  # Hide driver table
-        team_table.pack()  # Show team table
-        
+        # hide the driver table and show the team table
+        driver_table.pack_forget()
+        team_table.pack()
+
+        # clear the existing rows and readjust the table height
         team_table.delete(*team_table.get_children())
         team_table.config(height=len(result_dict))
 
+        # insert team's information into the table 
         for key, values in result_dict.items():
             team_table.insert('', 'end', values=(key, values['team'], values['points']))
 
     
 
 
-
+# create the main GUI window
 window = tk.Tk()
 window.geometry('1200x800')
 window.configure(bg='light blue')
 window.title('F1 Result Table')
 
+# title label
 title_label = tk.Label(window, text='F1 Results Table')
 title_label.configure(font=('ariel', 30), bg='light blue')
 title_label.pack(pady=(15,10))
 
+# instructional label
 entry_label = tk.Label(window, text='Select a category and enter a year below')
 entry_label.configure(font=('ariel', 12), bg='light blue')
 entry_label.pack()
 
-
+# input box for the year input
 entry = tk.Entry(width = 40)
 entry.config(font=('ariel', 10))
 entry.pack(pady=(0, 10))
 
+# dropdown box to choose a category, either drivers, teams or races
+# note: races category has not been implemented yet and is currently not functional
 categories = StringVar()
 drop = OptionMenu(window, categories, 'drivers', 'team', 'races')
 categories.set('drivers')
 drop.config(font=('ariel',12))
 drop.pack(pady=(0, 10))
 
-
-
+# create submit button
 submit = tk.Button(window, text='submit', command=submit)
 submit.pack()
 
+# create error label, set to blank for initial setup 
 error_label = tk.Label(window, text='')
 error_label.pack(pady=(10,5))
 error_label.configure(bg='light blue', fg='red', font=('ariel', 15))
 
-
-# table = ttk.Treeview(window, columns=('position', 'driver', 'team', 'points'), show='headings')
-# table.config(height=5)
-# table.pack(pady=(10, 0))
-# table.heading('position', text = 'position')
-# table.heading('driver', text = 'driver')
-# table.heading('team', text = 'team')
-# table.heading('points', text = 'points')
-
-
+# table for drivers result
 driver_table = ttk.Treeview(window, columns=('Position', 'Driver', 'Team', 'Points'), show='headings')
 driver_table.heading('Position', text='Position')
 driver_table.heading('Driver', text='Driver')
@@ -100,7 +118,7 @@ driver_table.column('Driver', width=180)
 driver_table.column('Team', width=230)
 driver_table.column('Points', width=66) 
 
-
+# table for teams result
 team_table = ttk.Treeview(window, columns=('Position', 'Team', 'Points'), show='headings')
 team_table.heading('Position', text='Position')
 team_table.heading('Team', text='Team')
@@ -110,12 +128,12 @@ team_table.column('Position', width=65, anchor=tk.CENTER)
 team_table.column('Team', width=230)
 team_table.column('Points', width=66) 
 
-
+# style for table row
 style = ttk.Style()
 style.configure("Treeview", background='#ededed')
 
 
-
+# run the GUI window
 window.mainloop()
 
 
--- scraper.py
+++ scraper.py
@@ -3,27 +3,43 @@ import requests
 
 
 def scraping(user_input, selected_category):
+    '''
+    scraps either all driver's or team's results from the website based on a given year
 
+    input:
+        - selected_category: containing whether ther user is request for the drivers or team results
+        - user_input: the year the results are based on
+    
+    output:
+        - a dictionary where it's key represents the leaderboard position and its value containing a list representing the 
+        information of the driver or the team
+    '''
+
+    # if no year is selected, return none
     if user_input == '':
         return None
     
+    # if the year provided is not numeric, return false
     elif not user_input.isdigit():
         return False
     
+    # craft the url based on the given category and year
     url = 'https://www.formula1.com/en/results.html/' + user_input + '/' + selected_category + '.html'
+
+    # Send a GET request to the URL and parse the HTML
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
     
-
+    # located the table on the page
     result_table = soup.find('table', class_ = 'resultsarchive-table')
 
-    
+    # if no table found, return false
     try:
         result_table.find_all('tbody')
     except AttributeError:
         return False
 
-
+    # call the appropriate scraping function based on the selected category
     if selected_category == 'drivers':
         return driver_scrap(result_table)
     elif selected_category == 'team':
@@ -31,80 +47,73 @@ def scraping(user_input, selected_category):
         
 
 def driver_scrap(result_table):
-    result_dict = {}
-    for driver_result_table in result_table.find_all('tbody'):
-        rows = driver_result_table.find_all('tr')
+    '''
+    extract driver's information (driver name, team name and total points) from the result table
 
+    input:
+        - result_table
+    output:
+        a dictionary where each key represents a leaderboard position and their value containing a list with the driver's 
+        name, team name and total points scored
+    '''
+    result_dict = {}
+
+    # loop through each tbody in case there are multiple sections (in the case if website design changes)
+    for driver_result_table in result_table.find_all('tbody'):
+        
+        # loop through each row 
+        rows = driver_result_table.find_all('tr')
         for i, row in enumerate(rows):
 
-            # driver's name 
+            # obtain driver's name 
             first_name = row.find('span', class_='hide-for-tablet').text
             last_name = row.find('span', class_='hide-for-mobile').text
 
-            # team name 
+            # obtain team name 
             team = row.find('a', class_='grey semi-bold uppercase ArchiveLink').text
 
-            # points scored
+            # obtain total points scored
             points = row.find('td', class_='dark bold').text
 
-            # add all infomation to a lis, key being the leaderboard position
+            # store all driver's infomation into a dictionary, key representing the leaderboard position
             result_dict[i+1] = {
                 'driver' : first_name + " " + last_name,
                 'team' : team,
                 'points' : points
             }
+
     return result_dict
 
 def team_scrap(result_table):
-    result_dict = {}
-    for driver_result_table in result_table.find_all('tbody'):
-        rows = driver_result_table.find_all('tr')
+    '''
+    extract team's information (team name and total points) from the result table
 
+    input:
+        - result_table
+    output:
+        a dictionary where each key represents a leaderboard position and their value containing a list with the team's 
+        name and total points scored
+    '''
+    result_dict = {}
+
+    # loop through each tbody in case there are multiple sections (in the case if website design changes)
+    for driver_result_table in result_table.find_all('tbody'):
+
+        # loop through each row 
+        rows = driver_result_table.find_all('tr')
         for i, row in enumerate(rows):
 
-            # team name 
+            # obtain team name 
             team = row.find('a', class_='dark bold uppercase ArchiveLink').text
 
-            # points scored
+            # obtain total points scored
             points = row.find('td', class_='dark bold').text
 
-            # add all infomation to a lis, key being the leaderboard position
+            # store all team's infomation into a dictionary, key representing the leaderboard position
             result_dict[i+1] = {
                 'team' : team,
                 'points' : points
             }
+
     return result_dict
 
-
-
-# def race_scrap(result_table):
-#     result_dict = {}
-#     for driver_result_table in result_table.find_all('tbody'):
-#         rows = driver_result_table.find_all('tr')
-
-#         for i, row in enumerate(rows):
-
-#             # grand prix name
-#             gp = row.find('a', class_='dark bold ArchiveLink').text
-
-#             # date
-#             date = row.find('td', class_='dark hide-for-mobile').text
-
-
-#             # winner's name 
-#             first_name = row.find('span', class_='hide-for-tablet').text
-#             last_name = row.find('span', class_='hide-for-mobile').text
-
-#             # team name 
-#             team = row.find('td', class_='semi-bold uppercase ').text
-
-#             # laps completed
-#             points = row.find('td', class_='bold hide-for-mobile').text
-
-#             # add all infomation to a lis, key being the leaderboard position
-#             result_dict[i+1] = {
-#                 'driver' : first_name + " " + last_name,
-#                 'team' : team,
-#                 'points' : points
-#             }
-#     return result_dict
